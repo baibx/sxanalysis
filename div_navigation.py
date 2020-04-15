874d95ca17eebed00aeafc37ecbf39221858360b
baibx <baibx1337@gmail.com>
2020-04-15 00:53:57 -0400

    lets goooooo

--- div_navigation.py
+++ div_navigation.py
@@ -11,4 +11,18 @@ soup = bs.BeautifulSoup(webpage, 'lxml')
 
 mobile = soup.find('ul', class_='list-unstyled sneakers')
 
-print(mobile.text)
+for size in mobile.find_all('div', class_="inset"):
+
+    size1 = size.find('div', class_="title")
+    size_price = size.find('div', class_="subtitle")
+
+    size1 = size1.text
+    size_price = size_price.text
+
+    print(size1 + " " + size_price)
+
+    print(" ")
+
+
+
+
