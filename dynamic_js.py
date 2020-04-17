009e0cdbbd1ed475a7a4a308889851b51ce599ac
baibx <baibx1337@gmail.com>
2020-04-17 00:31:07 -0400

    crashes, needs QApplication as a global variable

--- dynamic_js.py
+++ dynamic_js.py
@@ -1,32 +1,31 @@
-import bs4 as bs
 import sys
-import urllib.request
-from PyQt5.QtWebEngineWidgets import QWebEnginePage
 from PyQt5.QtWidgets import QApplication
 from PyQt5.QtCore import QUrl
+from PyQt5.QtWebEngineWidgets import QWebEnginePage
+import bs4 as bs
 
-class Page(QWebEnginePage):
+
+
+class Client(QWebEnginePage):
+
     def __init__(self, url):
         self.app = QApplication(sys.argv)
         QWebEnginePage.__init__(self)
         self.html = ''
-        self.loadFinished.connect(self._on_load_finished)
+        self.loadFinished.connect(self.on_page_load)
         self.load(QUrl(url))
         self.app.exec_()
 
-    def _on_load_finished(self):
-        self.html = self.toHtml(self.Callable)
-        print('Load finished')
 
-    def Callable(self, html_str):
-        self.html = html_str
+    def on_page_load(self):
         self.app.quit()
 
 
-def main():
-    page = Page('https://pythonprogramming.net/parsememcparseface/')
-    soup = bs.BeautifulSoup(page.html, 'html.parser')
-    js_test = soup.find('p', class_='jstest')
-    print(js_test.text)
+url = input("Link?")
+client_response = Client(url)
+soup = bs.BeautifulSoup(client_response.html, 'html.parser')
+js_test = soup.find('table', class_='activity-table table table-condensed table-striped ')
+print(js_test.text)
 
-if __name__ == '__main__': main()
+
+
