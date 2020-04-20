9afc7a26f7a7954e5a361303a593e63dca76189c
baibx <baibx1337@gmail.com>
2020-04-19 22:24:54 -0400

    added closing to the driver when process is completed, also included stockx login

--- dynamic_js_v2.py
+++ dynamic_js_v2.py
@@ -2,10 +2,37 @@ import requests
 from bs4 import BeautifulSoup
 from selenium import webdriver
 
+#login
+username = 'kickmoki@gmail.com'
+password = 'DXChruoyVp'
+
+url = 'https://accounts.stockx.com/login?state=g6Fo2SBWQjdGMUtEaG9HQ2xRYWt6Q1M0NWY3M2lFY3U5SnFsYaN0aWTZIG40QWc2R1ppbERMVkRMTHhGdF9ZZHE2OUxXQ2RGam9Go2NpZNkgT1Z4cnQ0VkpxVHg3TElVS2Q2NjFXMER1Vk1wY0ZCeUQ&client=OVxrt4VJqTx7LIUKd661W0DuVMpcFByD&protocol=oauth2&audience=gateway.stockx.com&connection=production&redirect_uri=https%3A%2F%2Fstockx.com%2Fcallback%3Fpath%3D%2F&response_type=code&stockx-currency=USD&stockx-default-tab=login&stockx-is-gdpr=&stockx-language=en&stockx-session-id=&stockx-url=https%3A%2F%2Fstockx.com&stockx-user-agent=Mozilla%2F5.0%20(Windows%20NT%2010.0%3B%20Win64%3B%20x64%3B%20rv%3A75.0)%20Gecko%2F20100101%20Firefox%2F75.0'
 execute = r'C:\Users\baizh\AppData\Local\Programs\Python\Python38-32\geckodriver.exe'
+
 options = webdriver.FirefoxOptions()
 options.add_argument('-headless')
 driver = webdriver.Firefox(executable_path=execute, firefox_options=options)
+
+driver.get(url)
+
+import time
+time.sleep(2)
+
+print("Logging in!")
+login = driver.find_element_by_xpath('//*[@id="email-login"]')
+login.click()
+login.send_keys(username)
+print("Username Filled!")
+
+print("Sending Password!")
+password_send = driver.find_element_by_xpath('//*[@id="password-login"]')
+password_send.click()
+password_send.send_keys(password)
+
+login_box = driver.find_element_by_xpath('//*[@id="login-button-text"]')
+login_box.click()
+print("Logged in!")
+
 url = 'https://stockx.com/air-jordan-1-retro-high-court-purple-white'
 source = requests.get(url)
 soup = BeautifulSoup(source.text, 'html.parser')
@@ -28,6 +55,8 @@ for tr in view_all.find('tbody'):
     split = tr.text
     print(split)
 
+driver.quit()
+print("Program exited")
 #split into two with the year
 #upload results to a dictionary
 #call the dictionary
